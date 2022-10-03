0aeee38fff51442a6b17298c6ccd88184076d5e7
Luke Patterson <lukepatterson31@gmail.com>
2022-10-03 10:56:51 +0100

    otel configuration, web app with basic hello world tracing

--- TriggerReportTest/Program.fs
+++ TriggerReportTest/Program.fs
@@ -1,20 +1,55 @@
 open System
-open System.Diagnostics
+open System.Threading.Tasks
+
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Hosting
-open OpenTelemetry
+open Microsoft.Extensions.DependencyInjection
+
+open OpenTelemetry.Exporter
 open OpenTelemetry.Resources
 open OpenTelemetry.Trace
 
-[<EntryPoint>]
-let main (args: string[]) =
-    let serviceName = "trigger-reports"
-    let builder = WebApplication.CreateBuilder(args)
-    builder.Services
+let builder = WebApplication.CreateBuilder()
 
-    let app = builder.Build()
+// Backend config
+let serviceName = "trigger-reports"
+let exporterEndpoint = "http://127.0.0.1:4317"
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+// Configure an exporter with some important info:
+//
+// - endpoint stuff you might need (e.g., headers)
+// - make sure the service name is set up
+// - configure some automatic instrumentation
+builder.Services.AddOpenTelemetryTracing(fun builder ->
+    builder
+        .AddSource(serviceName)
+        .AddOtlpExporter(fun otlpOptions ->
+            otlpOptions.Endpoint <- Uri exporterEndpoint
+            otlpOptions.Protocol <- OtlpExportProtocol.Grpc)
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+        .AddAspNetCoreInstrumentation(fun options ->
+            options.RecordException <- true)
+        .AddHttpClientInstrumentation()
+        //.AddSqlClientInstrumentation( fun opt ->
+        //    opt.SetDbStatementForText <- true
+        //    opt.RecordException <- true)
+            |> ignore
+        ) |> ignore
 
-    app.Run()
-    0
+// Start a tracer scoped to the service
+let tracer = TracerProvider.Default.GetTracer(serviceName)
+
+// Add the handler to the root route using .NET 6 APIs!
+let app = builder.Build()
+app.MapGet("/", Func<Task<string>>(fun () -> task {
+        // Track the work done in the root HTTP handler
+        use span = tracer.StartActiveSpan("sleep span")
+        span.SetAttribute("duration_ms", 100) |> ignore
+
+        do! Task.Delay(100)
+
+        return "Hello World!"
+    }) ) |> ignore
+
+app.Run()
